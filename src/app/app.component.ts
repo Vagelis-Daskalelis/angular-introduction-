b022321ca6282517ce0e9081c4cc182e7b41d44f
VAGELIS. DASK <vagelisdaskalelis@gmail.com>
2024-03-29 19:14:10 +0200

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,19 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name = "Vagelis"
 
+  person0: Person = {
+    givenName: 'Vagelis',
+    surName: 'Daskalelis',
+    age: 25,
+    email: 'vagelis.aueb@.gr',
+    address: 'Athens, Greece',
+  };
+
+  person1: Person = {
+    givenName: 'John',
+    surName: 'Doe',
+    age: 55,
+    email: 'john.aueb@.gr',
+    address: 'NEW YORK, USA',
+  };
 }
